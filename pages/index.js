bbf2122965bca4a067458b07400817b58d7c551d
sasioverlord1 <zxmo20041007@gmail.com>
2025-05-13 17:06:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: 20, fontFamily: 'Arial' }}>
-      <h1>🐳 WhalePumpObserver 已部署成功</h1>
-      <p>未来可通过 API / 数据可视化组件接入 webhook 报警展示。</p>
+      <h1>🚀 WhalePumpObserver 已部署成功</h1>
+      <p>未来可通过 API / 数据可视化组件接入、webhook 报警展示。</p>
     </div>
-  )
+  );
 }
