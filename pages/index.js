53b47f9bc8124d795b46f3de945206f8e9e28ac8
sasioverlord1 <zxmo20041007@gmail.com>
2025-05-13 17:10:49 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20, fontFamily: 'Arial' }}>
-      <h1>🚀 WhalePumpObserver 已部署成功</h1>
-      <p>未来可通过 API / 数据可视化组件接入、webhook 报警展示。</p>
-    </div>
-  );
-}
